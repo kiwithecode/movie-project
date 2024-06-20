d8741efb8283357be1d10e7453329f7675325bfb
kiwithecode <wowcelodion@gmail.com>
2024-06-20 13:17:52 -0500

    consumo de la api

--- src/services/tmdb.ts
+++ src/services/tmdb.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-const API_KEY = 'your_tmdb_api_key';
+const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMWM2MThlNDE0ZWQ4ODM2YTUxMTJlYzk4MzQzZjJmNSIsInN1YiI6IjY2NzQ2ODdmZThlNDM0NDU3NmEzY2NlYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BaIwLj-ckjJlj3sZq3y-4ajq2DlGS9ou-RGQcXYa5-I';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const tmdb = axios.create({
   baseURL: BASE_URL,
-  params: {
-    api_key: API_KEY,
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
   },
 });
 
 export const getTopRatedMovies = async () => {
-  const response = await tmdb.get('/movie/top_rated');
+  const response = await tmdb.get('/movie/top_rated?language=en-US&page=1');
   return response.data.results;
 };
 
