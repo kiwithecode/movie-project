5f3d3d3802f2c3423f4a9d449991fa094c5e5f70
kiwithecode <wowcelodion@gmail.com>
2024-06-20 17:09:53 -0500

    cards centrados

--- src/components/common/MovieCard.tsx
+++ src/components/common/MovieCard.tsx
@@ -5,10 +5,10 @@ import RatingStars from './RatingStars';
 
 const MovieCard: FC<Movie> = ({ id, title, poster_path, vote_average }) => {
   return (
-    <div className="max-w-xs rounded overflow-hidden shadow-lg bg-white dark:bg-gray-700 text-black dark:text-white">
+    <div className="max-w-xs rounded overflow-hidden shadow-lg bg-white dark:bg-gray-700 text-black dark:text-white mx-1 my-2">
       <Link to={`/movie/${id}`}>
         <img className="w-full" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
-        <div className="px-6 py-4">
+        <div className="px-4 py-2">
           <div className="font-bold text-xl mb-2">{title}</div>
           <RatingStars rating={vote_average} />
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,14 +26,14 @@ const Home = () => {
   }, [searchTerm, setPage]);
 
   return (
-    <div className="container mx-auto px-4 bg-light-background dark:bg-dark-background text-light-text dark:text-dark-text">
+    <div className="container mx-auto px-4 flex flex-col items-center">
       <h1 className="text-3xl font-bold my-8">Top Rated Movies</h1>
       <input
         type="text"
         placeholder="Search for a movie..."
         value={searchTerm}
         onChange={handleSearchChange}
-        className="p-2 mb-4 border rounded w-full bg-light-surface dark:bg-dark-surface text-light-text dark:text-dark-text"
+        className="p-2 mb-4 border rounded w-full bg-white dark:bg-gray-700 dark:text-white"
       />
       {loading ? (
         <div>Loading...</div>
@@ -41,7 +41,7 @@ const Home = () => {
         <div>No results found for "{searchTerm}"</div>
       ) : (
         <>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {movies.map((movie) => (
               <MovieCard
                 key={movie.id}
@@ -52,7 +52,7 @@ const Home = () => {
               />
             ))}
           </div>
-          <div className="w-full overflow-x-auto">
+          <div className="w-full overflow-x-auto mt-4">
             <Pagination
               currentPage={page}
               totalPages={totalPages}
