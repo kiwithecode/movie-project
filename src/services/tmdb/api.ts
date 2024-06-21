31e815e0c66f15e25e2811c25f5f7371ca95c64c
kiwithecode <wowcelodion@gmail.com>
2024-06-20 23:13:33 -0500

    buenas practicas de api key

--- src/services/tmdb/api.ts
+++ src/services/tmdb/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMWM2MThlNDE0ZWQ4ODM2YTUxMTJlYzk4MzQzZjJmNSIsInN1YiI6IjY2NzQ2ODdmZThlNDM0NDU3NmEzY2NlYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BaIwLj-ckjJlj3sZq3y-4ajq2DlGS9ou-RGQcXYa5-I';
+const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const tmdb = axios.create({
