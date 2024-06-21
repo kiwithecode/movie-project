1cc4dfecbaa1bb9ac12a18487634aa19422275f8
kiwithecode <wowcelodion@gmail.com>
2024-06-20 20:57:01 -0500

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,23 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
