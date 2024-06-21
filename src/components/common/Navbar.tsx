13370caf59f3257a7aa14c3bd7db1681b33e0920
kiwithecode <wowcelodion@gmail.com>
2024-06-21 09:15:25 -0500

    fix logo modo oscuro

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useTheme } from '../../context/ThemeContext';
-import { usePagination } from '../../context/PaginationContext';
-import logo from '../../assets/logo.png'; // Asegúrate de que esta ruta sea correcta
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "../../context/ThemeContext";
+import { usePagination } from "../../context/PaginationContext";
+import logoLight from "../../assets/logo.png";
+import logoDark from "../../assets/logodark.png";
 
 const Navbar: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
@@ -11,18 +12,34 @@ const Navbar: React.FC = () => {
 
   const handleHomeClick = () => {
     setPage(1);
-    navigate('/');
+    navigate("/");
   };
 
   return (
     <nav className="p-2 flex justify-between items-center bg-light-surface dark:bg-dark-surface">
-      <div onClick={handleHomeClick} className="cursor-pointer flex items-center">
-        <img src={logo} alt="Logo" className="logo" />
+      <div
+        onClick={handleHomeClick}
+        className="cursor-pointer flex items-center"
+      >
+        <img src={theme === "dark" ? logoDark : logoLight} alt="Logo" className="logo" />
       </div>
       <div className="flex items-center">
-        <span className={`iconify ${theme === 'dark' ? 'icon-[meteocons--clear-night]' : 'icon-[meteocons--clear-day]'} text-2xl cursor-pointer`} onClick={toggleTheme}></span>
+        <span
+          className={`iconify ${
+            theme === "dark"
+              ? "icon-[meteocons--clear-night]"
+              : "icon-[meteocons--clear-day]"
+          } text-2xl cursor-pointer`}
+          onClick={toggleTheme}
+        ></span>
         <label htmlFor="toggle" className="relative inline-block w-10 ml-2">
-          <input type="checkbox" id="toggle" checked={theme === 'dark'} onChange={toggleTheme} className="sr-only" />
+          <input
+            type="checkbox"
+            id="toggle"
+            checked={theme === "dark"}
+            onChange={toggleTheme}
+            className="sr-only"
+          />
           <div className="block bg-gray-600 w-10 h-6 rounded-full"></div>
           <div className="dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition transform duration-200 ease-in-out"></div>
         </label>
