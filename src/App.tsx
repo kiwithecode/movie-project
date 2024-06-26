4517f11f273d7800678f99caf0686c698dffadd4
Kevin Armas <karmas@libelulasoft.com>
2024-06-25 19:15:44 -0500

    workflow43

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/common/Navbar";
-import Footer from "./components/common/Footer"; 
+import Footer from "./components/common/Footer";
 import { useTheme } from "./context/ThemeContext";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   const { theme } = useTheme(); // Obtener el tema actual del contexto
-
+  //hola esta es mi rama salome
   return (
     <Router>
       <div
